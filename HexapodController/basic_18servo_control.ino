af572e68cbe9bed175d95e6778e924f0625b40e4
zschwid <61169156+zschwid@users.noreply.github.com>
2024-02-04 01:06:18 -0500

    Add files via upload

--- HexapodController/basic_18servo_control.ino
+++ HexapodController/basic_18servo_control.ino
@@ -1,103 +1,36 @@
 #include "HexapodController.h"
 
-void move_to_pos_at_speed(double position_value, double speed_value);
-Axis l1_m1(2, -3.14, 3.14);
-Axis l1_m2(3, -3.14, 3.14);
-Axis l1_m3(4, -3.14, 3.14);
-
-Axis l2_m1(5, -3.14, 3.14);
-Axis l2_m2(6, -3.14, 3.14);
-Axis l2_m3(7, -3.14, 3.14);
-
-Axis l3_m1(8, -3.14, 3.14);
-Axis l3_m2(9, -3.14, 3.14);
-Axis l3_m3(10, -3.14, 3.14);
-
-Axis l4_m1(11, -3.14, 3.14);
-Axis l4_m2(12, -3.14, 3.14);
-Axis l4_m3(13, -3.14, 3.14);
-
-Axis l5_m1(14, -3.14, 3.14);
-Axis l5_m2(15, -3.14, 3.14);
-Axis l5_m3(18, -3.14, 3.14);
-
-Axis l6_m1(19, -3.14, 3.14);
-Axis l6_m2(22, -3.14, 3.14);
-Axis l6_m3(23, -3.14, 3.14);
+Axis axes[6][3] = {
+    {Axis(2, -PI * 0.75, PI * 0.75), Axis(3, -PI * 0.75, PI * 0.75), Axis(4, -PI * 0.75, PI * 0.75)},
+    {Axis(5, -PI * 0.75, PI * 0.75), Axis(6, -PI * 0.75, PI * 0.75), Axis(7, -PI * 0.75, PI * 0.75)},
+    {Axis(8, -PI * 0.75, PI * 0.75), Axis(9, -PI * 0.75, PI * 0.75), Axis(10, -PI * 0.75, PI * 0.75)},
+    {Axis(11, -PI * 0.75, PI * 0.75), Axis(12, -PI * 0.75, PI * 0.75), Axis(13, -PI * 0.75, PI * 0.75)},
+    {Axis(14, -PI * 0.75, PI * 0.75), Axis(15, -PI * 0.75, PI * 0.75), Axis(18, -PI * 0.75, PI * 0.75)},
+    {Axis(19, -PI * 0.75, PI * 0.75), Axis(22, -PI * 0.75, PI * 0.75), Axis(23, -PI * 0.75, PI * 0.75)}
+};
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  l1_m1.set_mapping(0,1);
-  l1_m2.set_mapping(0,1);
-  l1_m3.set_mapping(0,1);
-  
-  l2_m1.set_mapping(0,1);
-  l2_m2.set_mapping(0,1);
-  l2_m3.set_mapping(0,1);
-  
-  l3_m1.set_mapping(0,1);
-  l3_m2.set_mapping(0,1);
-  l3_m3.set_mapping(0,1);
-  
-  l4_m1.set_mapping(0,1);
-  l4_m2.set_mapping(0,1);
-  l4_m3.set_mapping(0,1);
 
-  l5_m1.set_mapping(0,1);
-  l5_m2.set_mapping(0,1);
-  l5_m3.set_mapping(0,1);
-
-  l6_m1.set_mapping(0,1);
-  l6_m2.set_mapping(0,1);
-  l6_m3.set_mapping(0,1);
-}
-
-
-String remove_to_space(String command) {
-  while (command.charAt(0) != ' ' && command.charAt(0)) {
-    command.remove(0,1);
+  for (uint8_t i = 0; i < 6; i++) {
+    for (uint8_t j = 0; j < 3; j++) {
+      axes[i][j].set_mapping(1,0.75);
+    }
   }
-  command.remove(0,1);
-  return command;
 }
 
 void loop() {
-  double position_value;
+  double pos;
+  uint8_t leg;
+  uint8_t motor;
 
   if (Serial.available() > 0) {
     String command = Serial.readStringUntil('\n');
-    position_value = command.toFloat();
-
-    Serial.println(position_value);
-    move_to_pos(position_value);
-    Serial.println("lemon");
+    Serial.println("Received");
+    sscanf(command.c_str(), "%c %c %lf", &leg, &motor, &pos);
+    
+    Serial.printf("leg: %d; motor: %d; pos: %f", leg, motor, pos);
+    axes[leg][motor].move_to_pos(pos);
   }
 
-}
-
-// Function to update the Axis instance based on axis number, position value, and speed value
-void move_to_pos(double position_value) {
-  l1_m1.move_to_pos(position_value);
-  l1_m2.move_to_pos(position_value);
-  l1_m3.move_to_pos(position_value);
-  
-  l2_m1.move_to_pos(position_value);
-  l2_m2.move_to_pos(position_value);
-  l2_m3.move_to_pos(position_value);
-  
-  l3_m1.move_to_pos(position_value);
-  l3_m2.move_to_pos(position_value);
-  l3_m3.move_to_pos(position_value);
-  
-  l4_m1.move_to_pos(position_value);
-  l4_m2.move_to_pos(position_value);
-  l4_m3.move_to_pos(position_value);
-
-  l5_m1.move_to_pos(position_value);
-  l5_m2.move_to_pos(position_value);
-  l5_m3.move_to_pos(position_value);
-
-  l6_m1.move_to_pos(position_value);
-  l6_m2.move_to_pos(position_value);
-  l6_m3.move_to_pos(position_value);
 }
