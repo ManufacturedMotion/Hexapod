a7250dab9b466db218cf490c337319cb5691a707
zschwid <61169156+zschwid@users.noreply.github.com>
2024-02-03 21:41:47 -0500

    Add files via upload

--- HexapodController/leg.cpp
+++ HexapodController/leg.cpp
@@ -3,96 +3,63 @@
 #include <stdbool.h>
 #include <math.h>
 
-double * Leg::inverse_kinematics(double x, double y, double z) {
-    static double results[3];
-    if (fabs(y < 0.0005)) {
-        if (x > 0.0005) {
-            results[0] = PI / 2.0;
-        }
-        else if (x < -0.0005) {
-            results[0] = -PI / 2.0;
-        }
-        else {
-            results[0] = 0;
-        }
-    }  
-    else {
-        results[0] = atan2(x,y);
-    }
+// _Bool Leg::inverse_kinematics(double x, double y, double z) {
+//     double potential_results[3];
+//     if (fabs(y < 0.0005)) {
+//         if (x > 0.0005) {
+//             potential_results[0] = PI / 2.0;
+//         }
+//         else if (x < -0.0005) {
+//             potential_results[0] = -PI / 2.0;
+//         }
+//         else {
+//             potential_results[0] = 0;
+//         }
+//     }  
+//     else {
+//         potential_results[0] = atan2(x,y);
+//     }
 
-    double yv = sqrt(y * y + x * x);
-    double dt = sqrt(yv * yv + z * z);
+//     double yv = sqrt(y * y + x * x);
+//     double dt = sqrt(yv * yv + z * z);
     
-    double theta1_tool0 = atan2(yv,z);;
-    double theta1_tool1 = acos((dt * dt + _length1 * _length1 - _length2 * _length2) / (2 * dt * _length1));
+//     double theta1_tool0 = atan2(yv,z);;
+//     double theta1_tool1 = acos((dt * dt + _length1 * _length1 - _length2 * _length2) / (2 * dt * _length1));
     
-    double theta2_tool = acos((_length1 * _length1 + _length2 * _length2 - dt * dt) / (2 * _length1 * _length2));
+//     double theta2_tool = acos((_length1 * _length1 + _length2 * _length2 - dt * dt) / (2 * _length1 * _length2));
 
-    results[1] = theta1_tool0 - theta1_tool1;
-    results[2] = PI - theta2_tool;
+//     potential_results[1] = theta1_tool0 - theta1_tool1;
+//     potential_results[2] = PI - theta2_tool;
 
-    // results[1] = theta1_tool0 + theta1_tool1;
-    // results[2] = -(PI - theta2_tool);
+//     // potential_results[1] = theta1_tool0 + theta1_tool1;
+//     // potential_results[2] = -(PI - theta2_tool);
 
-    for (uint8_t i = 0; i < NUM_AXES; i++) {
-        if (results[i] != results[i]); //Check for NaN
-        return false;
-    }
-    for (uint8_t i = 0; i < NUM_AXES; i++) {
-        _next_angles[i] = results[i];
-    }
-    return results;
-}
+//     for (uint8_t i = 0; i < NUM_AXES; i++) {
+//         if (potential_results[i] != potential_results[i]); //Check for NaN
+//         return false;
+//     }
+//     for (uint8_t i = 0; i < NUM_AXES; i++) {
+//         _next_angles[i] = potential_results[i];
+//     }
+//     return true;
+// }
 
-_Bool Leg::rapid_move(double x,  double y, double z) {
-    double * positions = inverse_kinematics(x, y, z) 
-    move_axes(positions);
-    return true;
-}
+// _Bool Leg::rapid_move(double x,  double y, double z) {
+//     if (inverse_kinematics(x, y, z)) 
+//         move_axes();
+//         return true;
+//     return false;
+// }
 
-_Bool Leg::linear_move_setup(double x,  double y, double z, double speed) {
-    _move_time = (fabs(get_current_pos() - pos) / speed) * 1000;
-    _start_x = get_current_x();
-    _start_y = get_current_y();
-    _start_z = get_current_z();
-    _end_x = x;
-    _end_y = y;
-    _end_z = z;
-    _move_progress = 0;
-    _move_start_time = _millis;
-}
+// _Bool Leg::linear_move(double x,  double y, double z, double speed) {
+    
+// }
 
-
-uint8_t Leg::run_linear_move() {
-    _move_progress = (float)(_millis - _move_start_time) / ((float) _move_time);
-    if (_move_progress <= 1.0) {
-        double next_x = _move_progress * (_end_x - _start_x) + _start_x;
-        double next_y = _move_progress * (_end_x - _start_x) + _start_y;
-        double next_z = _move_progress * (_end_x - _start_x) + _start_z;
-        double * positions = inverse_kinematics(next_x, next_y, next_z); 
-        move_axes(inverse_kinematics(next_x, next_y, next_z));
-        return 0;
-    }
-    return 255;
-}
-
-void Leg::move_axes(double * positions) {
-    for (uint8_t i = 0; i < NUM_AXES; i++) {
-        axes[i].move_to_pos(_next_angles[i]);
-    }
-    for (uint8_t i = 0; i < NUM_AXES; i++) {
-        current_angles[i] = _next_angles[i];
-    }
-}
-
-double Leg::get_current_x() {
-    return current_cartesian[0];
-}
-
-double Leg::get_current_y() {
-    return current_cartesian[1];
-}
-
-double Leg::get_current_z() {
-    return current_cartesian[2];
-}
+// void Leg::move_axes() {
+//     for (uint8_t i = 0; i < NUM_AXES; i++) {
+//         axes[i].move_to_pos(_next_angles[i]);
+//     }
+//     for (uint8_t i = 0; i < NUM_AXES; i++) {
+//         current_angles[i] = _next_angles[i];
+//     }
+// }
--- HexapodController/leg.hpp
+++ HexapodController/leg.hpp
@@ -1,44 +1,27 @@
-#include <axis.hpp>
-#include <stdbool.h>
-#include <stdint.h>
+// #include <axis.hpp>
+// #include <stdbool.h>
+// #include <stdint.h>
 
-#ifndef LEG_H
-#define LEG_H
+// #ifndef LEG_H
+// #define LEG_H
 
 
-#define NUM_AXES 3
-class Leg {
-    public:
-        double current_angles[NUM_AXES];
-        double current_cartesian[NUM_AXES];
-        double get_current_x();
-        double get_current_y();
-        double get_current_z();
-        _Bool rapid_move(double x,  double y, double z);
-        _Bool linear_move(double x,  double y, double z, double speed);
-    private:
+// #define NUM_AXES 3
+// class Leg {
+//     public:
+//         double current_angles[NUM_AXES];
+//         double current_cartesian[NUM_AXES];
+//         _Bool rapid_move(double x,  double y, double z);
+//         _Bool linear_move(double x,  double y, double z, double speed);
+//     private:
+//         double _length0;
+//         double _length1;
+//         double _length2;
+//         _Bool move_axes();
+//         _Bool inverse_kinematics(double x, double y, double z);
+//         double _next_angles[NUM_AXES];
+//         double _next_cartesian[NUM_AXES];
+//         Axis axes[3];
+// };
 
-        //Inverse kinematics setup variables
-        double _length0;
-        double _length1;
-        double _length2;
-
-        //Linear movement variables
-        uint32_t _move_time;
-        double _start_x;
-        double _start_y;
-        double _start_z;
-        double _end_x;
-        double _end_y;
-        double _end_z;
-        double _move_progress;
-        uint32_t _move_start_time;
-
-        _Bool move_axes();
-        _Bool inverse_kinematics(double x, double y, double z);
-        double _next_angles[NUM_AXES];
-        double _next_cartesian[NUM_AXES];
-        Axis axes[3];
-};
-
-#endif
+// #endif
