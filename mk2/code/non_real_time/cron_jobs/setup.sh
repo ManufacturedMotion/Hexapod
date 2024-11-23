3c7bfdbac0535ef7146e41e8a6a437cba129b399
Dillon Kane <dkane3@binghamton.edu>
2024-11-23 17:25:57 -0500

    setup script to update crontab

---
+++ mk2/code/non_real_time/cron_jobs/setup.sh
@@ -0,0 +1,28 @@
+mkdir -p /scripts/
+
+for cronjob_dir in *; 
+    do 
+        if [ $cronjob_dir = "setup.sh" ]; then
+            continue 
+        fi
+        for file in $cronjob_dir/*; 
+            do
+                file=$(basename "${file}")
+                if [ $file = "cronjob" ]; then
+                    crontab -l > temp_cron
+                    crontask=$(cat $cronjob_dir/$file)
+                    escaped_crontask=$(printf '%s' "$crontask" | sed 's/[][\.*^$()|+?{}\\]/\\&/g')
+                    echo "escaped is $escaped_crontask"
+                    if grep -qF -- "$escaped_crontask" temp_cron; then
+                        continue
+                    else    
+                        echo "${crontask}" >> temp_cron
+                        crontab temp_cron
+                    fi
+                    rm temp_cron
+                else
+                    cp $cronjob_dir/$file /scripts/    
+                fi
+            done;
+    done;
+
