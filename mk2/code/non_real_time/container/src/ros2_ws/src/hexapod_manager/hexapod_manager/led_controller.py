dd333603dd206dccc3eae3130af1b5a4c521cb1b
Iacobacci <dannyji00@gmail.com>
2025-03-02 22:32:00 -0500

    add color changing button

--- mk2/code/non_real_time/container/src/ros2_ws/src/hexapod_manager/hexapod_manager/led_controller.py
+++ mk2/code/non_real_time/container/src/ros2_ws/src/hexapod_manager/hexapod_manager/led_controller.py
@@ -10,14 +10,12 @@ LED_COUNT = 60
 LED_PIN = board.SPI()
 DEBOUNCE_DELAY = 0.3
 
-# Colors
 off = (0, 0, 0, 0)
 colors = {
-    "pink": (245, 0, 87, 0), "red": (255, 0, 0, 0), "green": (0, 255, 0, 0), "blue": (0, 0, 255, 0),
-    "white": (0, 0, 0, 255), "purple": (106, 27, 154, 0), "lavender": (234, 128, 252, 0),
-    "light_blue": (0, 176, 255, 0), "cyan": (0, 172, 193, 0), "mint": (100, 255, 218, 0),
-    "lime": (118, 255, 3, 0), "yellow": (255, 255, 0, 0), "orange": (230, 81, 0, 0)
+    "red": (255, 0, 0, 0), "green": (0, 255, 0, 0), "blue": (0, 0, 255, 0),
+    "white": (0, 0, 0, 255), "purple": (106, 27, 154, 0), "yellow": (255, 255, 0, 0),
 }
+color_keys = list(colors.keys())
 
 class LEDController(Node):
     def __init__(self):
@@ -34,54 +32,63 @@ class LEDController(Node):
         self.pattern_index = 0
         self.running = True
         self.last_button_press = 0
+        self.color_index = 0
+        self.color_1 = color_keys[self.color_index]
         self.animation_thread = threading.Thread(target=self.run_animations, daemon=True)
         self.animation_thread.start()
-
-        self.get_logger().info("LED Controller Node Started")
 
     def clear_pixels(self):
         self.pixels.fill(off)
 
-    def loop(self, color=colors["red"], width=1, delay=0.005, reverse=False):
-        for _ in range(3):
-            for pixel in range(LED_COUNT):
-                if not self.running:
-                    return
-                time.sleep(delay)
-                index = LED_COUNT - 1 - pixel if reverse else pixel
-                off_index = index + 1 if reverse else index - 1
-                self.pixels[off_index % LED_COUNT] = off
-                for i in range(width):
-                    self.pixels[(index - i) % LED_COUNT if reverse else (index + i) % LED_COUNT] = color
-        self.clear_pixels()
+    def loop(self, color, delay=0.005, reverse=False):
+        color_val = colors.get(color, off)
+        width = 3
+        
+        for _ in range(LED_COUNT):
+            if not self.running:
+                return
+            
+            for i in range(width):
+                index = (-_ - i) % LED_COUNT if reverse else (_ + i) % LED_COUNT
+                self.pixels[index] = color_val
 
-    def windmill(self, color_1=colors["orange"], color_2=colors["purple"], delay=0.05, sections=4, reverse=False):
-        for _ in range(2):
+            time.sleep(delay)
+
+            tail_index = (-_ - width) % LED_COUNT if reverse else (_ - 1) % LED_COUNT
+            self.pixels[tail_index] = off
+
+    def windmill(self, color, delay=0.05, sections=4, reverse=False):
+        primary_color = colors.get(color, off)
+        secondary_color = colors["white"]
+
+        for _ in range(1):
             for pixel in range(LED_COUNT):
                 if not self.running:
                     return
                 time.sleep(delay)
+
                 index = LED_COUNT - 1 - pixel if reverse else pixel
+
                 for section in range(sections):
-                    color = color_1 if section % 2 == 0 else color_2
-                    self.pixels[(index + section * (LED_COUNT // sections)) % LED_COUNT] = color
-        self.clear_pixels()
+                    section_color = primary_color if section % 2 == 0 else secondary_color
+                    section_index = (index + section * (LED_COUNT // sections)) % LED_COUNT
+                    self.pixels[section_index] = section_color
 
-    def pattern(self, color=colors["red"], width=1, delay=0.2):
+    def pattern(self, color, width=1, delay=0.2):
+        color_val = colors.get(color, off)
         pixel_group = [i for i in range(0, LED_COUNT, 2 * width)]
         for _ in range(4):
             if not self.running:
                 return
             time.sleep(delay)
-            self.pixels.fill(color)
+            self.pixels.fill(color_val)
             for pixel in pixel_group:
                 self.pixels[pixel] = off
             time.sleep(delay)
-            self.pixels.fill(color)
+            self.pixels.fill(color_val)
             for pixel in range(LED_COUNT):
                 if pixel not in pixel_group:
                     self.pixels[pixel] = off
-        self.clear_pixels()
 
     def joy_callback(self, msg):
         current_time = time.time()
@@ -98,15 +105,23 @@ class LEDController(Node):
             self.running = True
             self.animation_thread = threading.Thread(target=self.run_animations, daemon=True)
             self.animation_thread.start()
+        
+        if len(msg.buttons) > 11 and msg.buttons[11] == 1:
+            if current_time - self.last_button_press < DEBOUNCE_DELAY:
+                return
+            
+            self.last_button_press = current_time
+            self.color_index = (self.color_index + 1) % len(color_keys)
+            self.color_1 = color_keys[self.color_index]
 
     def run_animations(self):
         while self.running:
             if self.pattern_index == 0:
-                self.loop()
+                self.loop(self.color_1)
             elif self.pattern_index == 1:
-                self.windmill(color_1=colors["mint"], color_2=colors["red"])
+                self.windmill(self.color_1)
             elif self.pattern_index == 2:
-                self.pattern()
+                self.pattern(self.color_1)
 
     def stop(self):
         self.running = False
