dae6fcd977a49f54bd225dbbbec81b8f76bfb329
Iacobacci <diacoba1@binghamton.edu>
2025-03-10 23:29:15 -0400

    more display updates

--- mk2/code/non_real_time/container/src/idle_entry_point.sh
+++ mk2/code/non_real_time/container/src/idle_entry_point.sh
@@ -4,17 +4,6 @@
 # This entrypoint can be specified to just have the container running indefinitely in idle
 ##########################################################################################
 
-# Find the mutter-Xwaylandauth file and symlink it to the correct location
-AUTH_FILE=$(find /run/user/1002/ -name 'mutter-Xwaylandauth*' | head -n 1)
-
-if [ -n "$AUTH_FILE" ]; then
-  ln -sf "$AUTH_FILE" /root/.Xauthority
-else
-  echo "Xwayland auth file not found!"
-  exit 1  # Exit if the file is not found
-fi
-
-# Keep the container running indefinitely in idle
 while :
 do
     sleep 1
