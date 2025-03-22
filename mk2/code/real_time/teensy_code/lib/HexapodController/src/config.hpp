bc1a5ad33322a58b628e9286238fe20dd66abc2e
zschwid <zschwid@gmail.com>
2025-03-22 08:23:25 -0400

    update config

--- mk2/code/real_time/teensy_code/lib/HexapodController/src/config.hpp
+++ mk2/code/real_time/teensy_code/lib/HexapodController/src/config.hpp
@@ -26,7 +26,7 @@
 
 	#ifdef ZACK
 	
-		#define PWM_PINS {{5, 6, 7}, {4, 3, 2}, {9, 10, 8},  {12, 13, 11}, {15, 14, 18}, {23, 19, 22}}
+		#define PWM_PINS {{2, 3, 4}, {5, 6, 7}, {8, 9, 10},  {11, 12, 13}, {14, 15, 18}, {19, 22, 23}}
 		#define ZERO_POINTS {{2.55, 2.4, 1.4}, {2.4, 2.25, 1.5}, {2.1, 2.4, 1.6}, {1.8, 2.35, 1.4}, {1.95, 2.3, 1.53}, {2.6, 2.35, 1.55}}
 		#define MAX_POS {{PI, PI, PI}, {PI, PI, PI}, {PI, PI, PI}, {PI, PI, PI}, {PI, PI, PI}, {PI, PI, PI}}
 		#define MIN_POS {{-PI, -PI, -PI}, {-PI, -PI, -PI}, {-PI, -PI, -PI}, {-PI, -PI, -PI}, {-PI, -PI, -PI}, {-PI, -PI, -PI}}
