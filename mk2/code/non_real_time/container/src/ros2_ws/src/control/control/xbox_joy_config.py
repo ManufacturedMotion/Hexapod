9b2353ad1d6c9a03fc77fdb2e3ea11685761571a
Dillon Kane <dkane3@binghamton.edu>
2025-06-28 20:25:03 -0400

    update Hexapod direction

--- mk2/code/non_real_time/container/src/ros2_ws/src/control/control/xbox_joy_config.py
+++ mk2/code/non_real_time/container/src/ros2_ws/src/control/control/xbox_joy_config.py
@@ -19,59 +19,59 @@ class XboxJoyNode(Node):
         self.poll_timer = self.create_timer(1.0 / self.poll_rate, self.poll_callback)
 
     def joy_callback(self, msg):
-        left_y = msg.axes[1]
-        left_x = msg.axes[0]
-        right_x = msg.axes[2]
-        right_y = msg.axes[3]
+        left_y = -msg.axes[0]
+        left_x = -msg.axes[1]
+        right_x = -msg.axes[3]
+        right_y = -msg.axes[2]
         dpad_horiz = msg.axes[6]
         dpad_vert = msg.axes[7]
 
         threshold = 0.3
 
         # --- RIGHT STICK: 8-way snap, only 0, ±0.5, ±1 ---
-        mag_right = math.hypot(right_x, right_y)
+        mag_right = math.hypot(right_y, right_x)
         discrete_x, discrete_y = 0.0, 0.0
         if mag_right > threshold:
             angle = math.atan2(right_y, right_x)
             direction = int(((math.degrees(angle) + 360 + 22.5) % 360) // 45)
             match direction:
-                case 0:      # Right
+                case 0:
                     discrete_x, discrete_y = 1.0, 0.0
-                case 1:      # Up-Right
+                case 1:
                     discrete_x, discrete_y = 0.5, 0.5
-                case 2:      # Up
+                case 2:
                     discrete_x, discrete_y = 0.0, 1.0
-                case 3:      # Up-Left
+                case 3:
                     discrete_x, discrete_y = -0.5, 0.5
-                case 4:      # Left
+                case 4:
                     discrete_x, discrete_y = -1.0, 0.0
-                case 5:      # Down-Left
+                case 5:
                     discrete_x, discrete_y = -0.5, -0.5
-                case 6:      # Down
+                case 6:
                     discrete_x, discrete_y = 0.0, -1.0
-                case 7:      # Down-Right
+                case 7:
                     discrete_x, discrete_y = 0.5, -0.5
 
-            discrete_x *= mag_right
             discrete_y *= mag_right
+            discrete_x *= mag_right
 
         # --- LEFT STICK: Only one dominant direction, never both ---
-        mag_left = math.hypot(left_x, left_y)
-        lin_y, ang_z = 0.0, 0.0
-        if abs(left_y) > threshold or abs(left_x) > threshold:
-            if abs(left_y) > abs(left_x):
-                lin_y = math.copysign(1.0, left_y)
+        mag_left = math.hypot(left_y, left_x)
+        lin_x, ang_z = 0.0, 0.0
+        if abs(left_x) > threshold or abs(left_y) > threshold:
+            if abs(left_x) > abs(left_y):
+                lin_x = math.copysign(1.0, left_x)
                 ang_z = 0.0
             else:
-                ang_z = math.copysign(1.0, left_x)
-                lin_y = 0.0
+                ang_z = math.copysign(1.0, left_y)
+                lin_x = 0.0
         
             ang_z *= mag_left
-            lin_y *= mag_left
+            lin_x *= mag_left
 
         twist = Twist()
-        twist.linear.x = discrete_x
-        twist.linear.y = discrete_y if mag_right > threshold else lin_y
+        twist.linear.y = discrete_y
+        twist.linear.x = discrete_x if mag_right > threshold else lin_x
         twist.angular.z = ang_z
         twist.angular.y = dpad_horiz
         twist.angular.x = dpad_vert
